971a242c7e14278fbaadb7f472b931850c631a80
Akash Ahmed <57281241+Akash-Ahmed-CSE@users.noreply.github.com>
2021-02-19 02:56:19 +0600

    Add files via upload

---
+++ URI problems/uri 2721.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a[9],i,p,sum=0;
+    for(i=0;i<9;i++){
+        scanf("%d",&a[i]);
+        sum+=a[i];
+    }
+    p=sum%9;
+    if (p==1)printf("Dasher\n");
+    else if(p==2)printf("Dancer\n");
+    else if(p==3)printf("Prancer\n");
+    else if(p==4)printf("Vixen\n");
+    else if(p==5)printf("Comet\n");
+    else if(p==6)printf("Cupid\n");
+    else if(p==7)printf("Donner\n");
+    else if(p==8)printf("Blitzen\n");
+    else if(p==0)printf("Rudolph\n");
+    return 0;
+}
---
+++ URI problems/uri1541.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,s;
+    double area;
+
+    for (;;){
+        scanf("%d",&a);
+        if(a==0)break;
+        scanf("%d %d",&b,&c);
+     area = a*b*100.0/c;
+    //for(s=1;s*s<=area;s++);
+    s=1;
+    while(s*s<=area){
+        s++;
+    }
+        s--;
+        printf("%d\n",s);
+
+
+    }
+    return 0;
+
+}
