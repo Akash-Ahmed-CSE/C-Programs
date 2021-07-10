a67edc9933a88128dd12e7e5be79317b556b8fef
Akash Ahmed <57281241+Akash-Ahmed-CSE@users.noreply.github.com>
2021-07-10 22:47:11 +0600

    Add files via upload

---
+++ Guss the Output/True_False.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+
+    int a = 5+5;
+    int b = 10;
+    if(!a>=b){
+        printf("True");
+    }else printf("False");
+
+   printf(!(5+5>=10));
+   return 0;
+}
